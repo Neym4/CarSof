e7dd5a96275ede4f416ea12b18f83dcd1323278d
Neym4 <nikita08010456@mail.com>
2019-08-27 15:30:56 +0700

    SoftCar2

--- vievs/MainForm.py
+++ vievs/MainForm.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\admin\PycharmProjects\CarSoft\vievs\softForCar.ui'
+# Form implementation generated from reading ui file 'C:\Users\admin\PycharmProjects\Exhibition-mjun1 (1)\Exhibition-mjun1\vievs\softForCar.ui'
 #
-# Created by: PyQt5 UI code generator 5.12.1
+# Created by: PyQt5 UI code generator 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
+
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -12,6 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_softForCar(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
         MainWindow.resize(1299, 711)
         MainWindow.setMinimumSize(QtCore.QSize(1299, 711))
         MainWindow.setMaximumSize(QtCore.QSize(1299, 34343))
@@ -28,31 +30,31 @@ class Ui_softForCar(object):
         self.battery.setInvertedAppearance(False)
         self.battery.setObjectName("battery")
         self.speedometer = QtWidgets.QLCDNumber(self.centralwidget)
-        self.speedometer.setGeometry(QtCore.QRect(30, 602, 111, 41))
+        self.speedometer.setGeometry(QtCore.QRect(1090, 600, 111, 41))
         self.speedometer.setObjectName("speedometer")
         self.ButtonToUp = QtWidgets.QPushButton(self.centralwidget)
-        self.ButtonToUp.setGeometry(QtCore.QRect(930, 500, 50, 50))
+        self.ButtonToUp.setGeometry(QtCore.QRect(90, 530, 50, 50))
         self.ButtonToUp.setMinimumSize(QtCore.QSize(50, 50))
         self.ButtonToUp.setMaximumSize(QtCore.QSize(75, 75))
         self.ButtonToUp.setObjectName("ButtonToUp")
         self.ButtonToBeck = QtWidgets.QPushButton(self.centralwidget)
-        self.ButtonToBeck.setGeometry(QtCore.QRect(930, 560, 50, 50))
+        self.ButtonToBeck.setGeometry(QtCore.QRect(90, 590, 50, 50))
         self.ButtonToBeck.setMinimumSize(QtCore.QSize(50, 50))
         self.ButtonToBeck.setMaximumSize(QtCore.QSize(75, 75))
         self.ButtonToBeck.setObjectName("ButtonToBeck")
         self.ButtonToLeft = QtWidgets.QPushButton(self.centralwidget)
-        self.ButtonToLeft.setGeometry(QtCore.QRect(870, 560, 50, 50))
+        self.ButtonToLeft.setGeometry(QtCore.QRect(30, 590, 50, 50))
         self.ButtonToLeft.setMinimumSize(QtCore.QSize(50, 50))
         self.ButtonToLeft.setMaximumSize(QtCore.QSize(75, 75))
         self.ButtonToLeft.setObjectName("ButtonToLeft")
         self.ButtonToRight = QtWidgets.QPushButton(self.centralwidget)
-        self.ButtonToRight.setGeometry(QtCore.QRect(990, 560, 50, 50))
+        self.ButtonToRight.setGeometry(QtCore.QRect(150, 590, 50, 50))
         self.ButtonToRight.setMinimumSize(QtCore.QSize(50, 50))
         self.ButtonToRight.setMaximumSize(QtCore.QSize(75, 75))
         self.ButtonToRight.setObjectName("ButtonToRight")
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(200, 60, 31, 31))
-        self.graphicsView.setObjectName("graphicsView")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(180, 60, 121, 31))
+        self.textEdit.setObjectName("textEdit")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1299, 21))
@@ -72,5 +74,3 @@ class Ui_softForCar(object):
         self.ButtonToBeck.setText(_translate("MainWindow", "PushButton"))
         self.ButtonToLeft.setText(_translate("MainWindow", "PushButton"))
         self.ButtonToRight.setText(_translate("MainWindow", "PushButton"))
-
-
